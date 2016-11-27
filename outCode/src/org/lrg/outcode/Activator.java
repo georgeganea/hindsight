5127537db6ef12c9b5dce3cc034a9948601ee8b0
George Ganea <georgeganea@gmail.com>
2016-11-27 23:10:58 +0200

    fixed plugin id in java file

--- outCode/src/org/lrg/outcode/Activator.java
+++ outCode/src/org/lrg/outcode/Activator.java
@@ -27,7 +27,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.lrg.OutCode"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.lrg.hindsight"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
